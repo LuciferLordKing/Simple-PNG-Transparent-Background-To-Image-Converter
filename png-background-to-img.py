e6063a805406e541feccac0bbaa298e2bf7b7bfe
Dipken Fung <23611770+LuciferLordKing@users.noreply.github.com>
2023-06-02 22:04:29 +0100

    Code Refactoring (Mainly Done By GPT Plus 😜

--- png-background-to-img.py
+++ png-background-to-img.py
@@ -2,43 +2,53 @@
 from PIL import Image
 import argparse, sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input-filename', help='The Full Input Filename (Must Be *png)')
-parser.add_argument('-a', '--background-opacity', help='The alpha value a of the background pixels where 0 ≤ a ≤ 255 (Default: 255)')
-parser.add_argument('-o', '--output-filename', help='The Full Output Filename (Default: shadow-art.png)')
+def get_input_filename():
+    return input('input-filename: ')
 
-if parser.parse_args().input_filename is None:
-    filename = input('input-filename: ')
-else:
-    filename = parser.parse_args().input_filename
+def get_background_opacity():
+    return int(input('background-opacity[255]: ') or 255)
 
-with Image.open(filename, 'r') as im:
-    if not im.filename.endswith('.png'):
-        sys.exit('File extension is not PNG.')
+def get_background_colour(opacity):
+    colour = input('background-colour[0, 0, 0]: ') or '0, 0, 0'
+    return tuple(map(int, colour.split(','))) + (opacity, )
 
-    if parser.parse_args().background_opacity is None:
-        opacity = int(input('background-opacity[255]: ') or 255)
-    else:
-        opacity = int(parser.parse_args().background_opacity)
+def get_image_colour():
+    colour = input('image-colour[0, 0, 0, 0]: ') or '0, 0, 0, 0'
+    return tuple(map(int, colour.split(',')))
 
-    pixels = im.load()
-    background_colour = tuple()
-    while len(background_colour) != 4:
-        background_colour = tuple(map(int, (input('background-colour[0, 0, 0]: ') or '0, 0, 0').split(','))) + (opacity, )
-    image_colour = tuple()
-    while len(image_colour) != 4:
-        image_colour = tuple(map(int, (input('image-colour[0, 0, 0, 0]: ') or '0, 0, 0, 0').split(',')))
+def get_output_filename():
+    return input('output-filename[shadow-art.png]: ') or 'shadow-art.png'
 
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            if pixels[i, j][3] == 0:
-                pixels[i, j] = background_colour
-            else:
-                pixels[i, j] = image_colour
+def process_image(input_filename, background_colour, image_colour, output_filename):
+    with Image.open(input_filename) as im:
+        if not im.filename.endswith('.png'):
+            raise ValueError('File extension is not PNG.')
 
-    if parser.parse_args().output_filename is None:
-        out = input('output-filename[shadow-art.png]: ') or 'shadow-art.png'
-    else:
-        out = parser.parse_args().output_filename
+        pixels = im.load()
 
-    im.save(out)
+        for i in range(im.size[0]):
+            for j in range(im.size[1]):
+                if pixels[i, j][3] == 0:
+                    pixels[i, j] = background_colour
+                else:
+                    pixels[i, j] = image_colour
+
+        im.save(output_filename)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-filename', help='The Full Input Filename (Must Be *png)')
+    parser.add_argument('-a', '--background-opacity', type=int, help='The alpha value "a" of the background pixels where 0 ≤ a ≤ 255 (Default: 255)')
+    parser.add_argument('-o', '--output-filename', help='The Full Output Filename (Default: shadow-art.png)')
+    args = parser.parse_args()
+
+    input_filename = args.input_filename or get_input_filename()
+    background_opacity = args.background_opacity or get_background_opacity()
+    background_colour = get_background_colour(background_opacity)
+    image_colour = get_image_colour()
+    output_filename = args.output_filename or get_output_filename()
+
+    process_image(input_filename, background_colour, image_colour, output_filename)
+
+if __name__ == '__main__':
+    main()
